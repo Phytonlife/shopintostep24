0002bd625a844d8173c5de73b42394c30224b1df
Phytonlife <r.asel@list.ru>
2023-07-28 13:01:53 +0500

    add to html template

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls.static import static
 from django.urls import path,re_path
 from base.views import *
 handler404 = pageNotFound
+# path('', mainpage,name="base"),
 urlpatterns = [
-    path('', mainpage,name="base"),
+    path('', page, name='home'),
+    path('about/', about, name='about'),
     path("birthday/<slug:birthid>/",categories,name="birthday"),
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive,name="archive")
 ]
--- base/views.py
+++ base/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-
+from .models import *
 # Create your views here.
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+def page(request):
+    return render(request,"base/page.html")
+def index(request):
+    posts = Product.objects.all()
+    return render(request, 'base/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+def about(request):
+    return render(request, 'base/about.html', {'menu': menu, 'title': 'О сайте'})
 def mainpage(request):
-    return render(request,"base/category.html")
+    return render(request,"base/base.html",{"menu":menu, 'title': 'Главная страница'})
 def categories(request,birthid):
-    if request.GET:
-        print(request.GET)
-    return render(request,"base/category.html")
+    objects=Product.objects.all()
+    return render(request,"base/category.html",{"objects":objects})
 def archive(request, year):
     # перенаправления сайта
     # if (int(year) > 2020):
